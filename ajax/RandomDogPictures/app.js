68871a4648d98561d6e74f786ea6198e48bec057
dev4m2 <dev4m2@users.noreply.github.com>
2022-02-17 17:22:18 +0000

    Modify app.js to serve files from 'public' folder

--- ajax/RandomDogPictures/app.js
+++ ajax/RandomDogPictures/app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// serve static files from the `public` folder
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
