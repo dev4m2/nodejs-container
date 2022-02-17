fd726034002a1b022492dd721eb52196d1ae5584
dev4m2 <dev4m2@users.noreply.github.com>
2022-02-17 19:15:57 +0000

    Append semicolons to code

--- ajax/RandomDogPictures/app.js
+++ ajax/RandomDogPictures/app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 // serve static files from the `public` folder
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
